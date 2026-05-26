00d86589eac451e12464634ce9e69161e2cad29e
vinuta1310 <vinuta123.hiremath@gmail.com>
2026-05-26 16:37:36 +0530

    Create migration

---
+++ db/migrate/20260526110041_create_patient.rb
@@ -0,0 +1,11 @@
+class CreatePatient < ActiveRecord::Migration[8.0]
+  def change
+    create_table :patients do |t|
+      t.string :name
+      t.integer :age
+      t.string :gender
+      
+      t.timestamps
+    end
+  end
+end
