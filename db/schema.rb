5bb1f31792c80ab2392f6767be759af87c080c33
Vinuta M Hiremath <vinuta123.hiremath@gmail.com>
2026-06-01 16:16:08 +0530

    change sqlite to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2026_05_31_084010) do
+  enable_extension "pg_catalog.plpgsql"
   create_table "doctors", force: :cascade do |t|
     t.string "doctor_name"
     t.string "specialization"
