19e7c0fcfb70c321922eb20d8456ae85307f9f57
Vinuta M Hiremath <vinuta123.hiremath@gmail.com>
2026-06-03 11:39:04 +0530

    Added new action, added form, made some changes in index file

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -8,10 +8,17 @@ class AppointmentsController < ApplicationController
   def show
   end
 
+  def new
+    @appointment = Appointment.new
+  end
+
   private
 
   def set_appointment
     @appointment = Appointment.find(params[:id])
   end
 
+  def set_appointment_params
+    params.require(:appointment).permit(:name, :doctor_name, :reason)
+  end
 end
